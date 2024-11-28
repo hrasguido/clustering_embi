146f27bd6f80afc126df4824556468bd66842b0c
Howard Rasguido <howard@shipedge.com>
2024-11-27 20:00:19 -0400

    update

--- em.py
+++ em.py
@@ -7,51 +7,69 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.mixture import GaussianMixture
 import matplotlib.pyplot as plt
+from sklearn.metrics import silhouette_score, silhouette_samples
 
-
-
-df = pd.read_excel('original_dataset_copy.xlsx')
-
-# informacion inicial del dataframe:
-
-#print(df.info())
-
-# tipos del dataframe
-
-#print(df.dtypes)
-
-# Visualización de datos faltantes
-
-#print(df.describe(include='all'))
-
-# manejo de datos faltantes (imputacion)
-
-df_cleaned = df.dropna()
-#print(df_cleaned)
-
-# identificacion de valores atipicos
-
-# df_box = df_cleaned.drop('Fecha', axis=1)
-# df_box = df_box.drop('Global', axis=1)
-# df_box = df_box.drop('LATINO', axis=1)
-# df_box = df_box.drop('RD-LATINO', axis=1)
-
-# print(df_box.head)
-
-# plt.figure(figsize=(12, 6))
-# df_box.boxplot()
-
-# plt.ylabel('EMBI+')
-# plt.xticks(rotation=45)
-
-# plt.tight_layout()
-# plt.show()
-
-# Reduccion de la dimensionalidad
-df_cleaned = df_cleaned.drop('Fecha', axis=1)
-df_cleaned = df_cleaned.drop('Global', axis=1)
-df_cleaned = df_cleaned.drop('LATINO', axis=1)
-df_cleaned = df_cleaned.drop('RD-LATINO', axis=1)
-df_cleaned = df_cleaned.drop('Venezuela', axis=1)
 
 df = pd.read_csv('clean_data.csv')
+
+
+X = df.values
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+range_n_clusters = [2, 3, 4, 5, 6]
+
+for n_clusters in range_n_clusters:
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.set_size_inches(18, 7)
+    
+    ax1.set_xlim([-0.1, 1])
+    ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+
+    clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+    cluster_labels = clusterer.fit_predict(X_scaled)
+
+    silhouette_avg = silhouette_score(X_scaled, cluster_labels)
+    
+    sample_silhouette_values = silhouette_samples(X_scaled, cluster_labels)
+
+    y_lower = 10
+    for i in range(n_clusters):
+        ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
+        ith_cluster_silhouette_values.sort()
+        
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+        
+        color = plt.cm.nipy_spectral(float(i) / n_clusters)
+        ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                          0, ith_cluster_silhouette_values,
+                          facecolor=color, edgecolor=color, alpha=0.7)
+        
+        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+        y_lower = y_upper + 10
+
+    ax1.set_xlabel("Coeficienta de silhouette")
+    ax1.set_ylabel("Etiqueta de clusters")
+    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+    ax1.set_yticks([])
+    ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    colors = plt.cm.nipy_spectral(cluster_labels.astype(float) / n_clusters)
+    ax2.scatter(X_scaled[:, 0], X_scaled[:, 1], marker='.', s=30, lw=0, alpha=0.7,
+                c=colors, edgecolor='k')
+
+    centers = clusterer.cluster_centers_
+    ax2.scatter(centers[:, 0], centers[:, 1], marker='o',
+                c="white", alpha=1, s=200, edgecolor='k')
+
+    for i, c in enumerate(centers):
+        ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1,
+                    s=50, edgecolor='k')
+
+    ax2.set_title("Clusters")
+    ax2.set_xlabel("Caracteristica 1")
+    ax2.set_ylabel("Caracteristica 2")
+
+
+plt.show()
